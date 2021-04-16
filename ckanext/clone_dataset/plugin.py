daad81f073e3914c1271ccf68486284c407766f6
Mark Calvert <markcalvert84@gmail.com>
2021-04-16 18:37:34 -0500

    [DDCI-771] - Only check user's permission for a group if the group is not an organisation and is not coming from clone_dataset

--- ckanext/clone_dataset/plugin.py
+++ ckanext/clone_dataset/plugin.py
@@ -1,12 +1,14 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.logic.auth.create as auth_create
 
-from ckanext.clone_dataset import blueprint
+from ckanext.clone_dataset import blueprint, overrides
 
 
 class CloneDatasetPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IConfigurable, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
@@ -17,3 +19,7 @@ class CloneDatasetPlugin(plugins.SingletonPlugin):
     # IBlueprint
     def get_blueprint(self):
         return blueprint.clone_dataset
+
+    # IConfigurable
+    def configure(self, config):
+        auth_create._check_group_auth = overrides._check_group_auth
